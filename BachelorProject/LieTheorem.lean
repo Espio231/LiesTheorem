2cfe12577408e34a54cb8b29f42837bbb64ef62d
Lucas Whitfield <lucaswhitfield@hotmail.com>
2024-01-31 18:49:25 +0000

    State Lie's Theorem

--- BachelorProject/LieTheorem.lean
+++ BachelorProject/LieTheorem.lean
@@ -1,26 +1,30 @@
 import Mathlib.Algebra.Lie.Solvable
-import Mathlib.LinearAlgebra.Matrix.Block
-import Mathlib.Algebra.Lie.Classical
+import Mathlib.Algebra.Lie.Weights.Linear
+import Mathlib.LinearAlgebra.Dual
 
+open LieAlgebra
 
-variable {R L n : Type*}
+-- let k be a field of characteristic zero
+variable (k : Type*) [Field k] [CharZero k]
+-- Let L be a Lie algebra over k
+variable (L : Type*) [LieRing L] [LieAlgebra k L]
+-- and let V be a finite-dimensional triangularizable k-representation of L
+variable (V : Type*) [AddCommGroup V] [Module k V] [FiniteDimensional k V]
+  [LieRingModule L V] [LieModule k L V] [LieModule.IsTriangularizable k L V] [Nontrivial V]
 
-variable [CommRing R] [LieRing L] [LieAlgebra R L]
+-- If `L` is nilpotent, we can find a non-zero eigenvector
+theorem LieModule.exists_forall_lie_eq_smul' [LieAlgebra.IsNilpotent k L] :
+    ∃ χ : Module.Dual k L, ∃ v : V, v ≠ 0 ∧ ∀ x : L, ⁅x, v⁆ = χ x • v := by
+  suffices ∃ χ : L → k, weightSpace V χ ≠ ⊥ by
+    obtain ⟨χ, hχ⟩ := this
+    let χ' : weight k L V := ⟨χ, (finite_weightSpace_ne_bot k L V).mem_toFinset.mpr hχ⟩
+    use weight.toLinear k L V χ'
+    exact exists_forall_lie_eq_smul_of_weightSpace_ne_bot k L V χ hχ
+  by_contra! contra
+  simpa [contra] using iSup_weightSpace_eq_top k L V
 
-def upperTriagModule [Fintype n] [LinearOrder n] : Submodule R (Matrix n n R) where
-  carrier := {m | ∀ i j : n, j < i → m i j = 0}
-  add_mem' := by
-    intro a b ha hb i j hji
-    rw [Matrix.add_apply, ha i j hji, hb i j hji, add_zero]
-  zero_mem' := fun _ _ _ ↦ Matrix.zero_apply _ _
-  smul_mem' := by
-    intro α m hm i j hji
-    rw [Matrix.smul_apply, hm i j hji, smul_zero]
-
-
-
-def upperTriag [Fintype n] [LinearOrder n] : LieSubalgebra R (Matrix n n R) :=
-  {upperTriagModule with
-    lie_mem' := sorry}
-
---
+-- But a better result, **Lie's theorem**, is true, namely:
+-- If `L` is solvable, we can find a non-zero eigenvector
+theorem LieModule.exists_forall_lie_eq_smul [IsSolvable k L] :
+    ∃ χ : Module.Dual k L, ∃ v : V, v ≠ 0 ∧ ∀ x : L, ⁅x, v⁆ = χ x • v := by
+  sorry
